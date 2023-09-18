c93ec55069463a0061806d241ca38a5c90ea9b02
David <grayd500@gmail.com>
2023-09-17 23:44:28 -0500

    add local storage

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function imageMode(color) {
     image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
+
 // Dark Mode Styles
 function darkMode() {
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
@@ -38,12 +39,25 @@ function lightMode() {
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage for Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
